6761f7deb26c78234875035647a388c124662883
HP <HP@LAPTOP-5TSFA7HT>
2023-08-26 07:17:18 +0530

    getTest() method is created by dev1

--- TestProject/src/com/nt/test/Test.java
+++ TestProject/src/com/nt/test/Test.java
@@ -1,5 +1,14 @@
 package com.nt.test;
 
-public class Test {
+import com.nt.beans.Utility;
 
+public class Test {
+	public static void getTest() {
+		System.out.println("From Test Class ---line 1");
+		System.out.println(new Utility().getDate());
+	}
+
+	public static void main(String[] args) {
+		Test.getTest();
+	}
 }
