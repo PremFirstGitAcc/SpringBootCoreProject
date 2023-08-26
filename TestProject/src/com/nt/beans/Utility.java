fa141c43894e3e875a93ed29b5f127f87503a9c5
HP <HP@LAPTOP-5TSFA7HT>
2023-08-26 07:00:58 +0530

    classes created

---
+++ TestProject/src/com/nt/beans/Utility.java
@@ -0,0 +1,14 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class Utility {
+
+	public Date getDate() {
+		return new Date();
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
